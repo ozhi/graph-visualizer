5c929e3d2fdd2d3173fd071abdfc4202349a8790
ozhi <bozhin@gmail.com>
2018-09-04 15:13:50 +0300

    Initial implementation

---
+++ priority-queue-traverse.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function dijkstra() {
+  // TODO: implement this
+  alert('Dijkstra\'s algorithm not implemented yet');
+}
+
+function aStar() {
+  // TODO: implement this
+  alert('A* not implemented yet');
+}
---
+++ traverse.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function dfs() {
+  alert('Depth-first search not implemented yet');
+}
+
+function bfs() {
+  alert('Breadth-first search not implemented yet');
+}
